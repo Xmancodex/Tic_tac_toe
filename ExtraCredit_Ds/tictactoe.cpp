3512ad7e02813b4dfe00df313986c38ec795444e
Xa-Quan Washington <xaquanwashington@gmail.com>
2024-12-03 21:35:49 -0600

    updated

--- ExtraCredit_Ds/tictactoe.cpp
+++ ExtraCredit_Ds/tictactoe.cpp
@@ -3,7 +3,6 @@
 #include <iomanip> 
 #include <fstream>
 using namespace std;  
-char board[3][3]{ {'0','1','2'},{'3','4','5'},{'6','7','8'} };
 int main() //the main function
 {
 	char board[3][3]{ {'0','1','2'},{'3','4','5'},{'6','7','8'} }; 
